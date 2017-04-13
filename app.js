5fd389ca408181676b98b8bdcaa51d437b4278e0
htwright <harrisontwright@gmail.com>
2017-04-13 15:02:53 -0500

    most core functionality in place

--- app.js
+++ app.js
@@ -1,20 +1,53 @@
 const apikey = 'a7afa68d4dc2512e1d7bcdf7a20c5a73';
-const endpointURL = "api.openweathermap.org/data/2.5/forecast/daily";
+const endpointURL = "http://api.openweathermap.org/data/2.5/forecast/daily";
 
 
 $(function(){
-
-  function getData(callback){
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth()+1;
+  function getData(userInput, userCnt, userUnits, callback){
     const query={
-      id:4699066,
-      key:apikey,
-      cnt:7
+      zip: `${userInput},US`,
+      apikey:apikey,
+      cnt:userCnt,
+      mode:'json',
+      units:userUnits
     }
+
     $.getJSON(endpointURL, query, callback);
   }
   function showData(data){
+    let title = data.city.name;
+    let dataHTML = ``;
     console.log(data);
+    let currentDate=dd;
+
+    data.list.map(function(obj){
+      dataHTML += '<h1>'+mm+'/'+currentDate+'</h1><ul><li>'+'Day time temperature: ' +obj.temp.day+'</li><br><li>'+obj.weather[0].main+'</li></ul>';
+      currentDate++;
+    })
+    $('.js-titleContainer').append(`<h1>${title}</h1>`)
+    $('.js-container').append(dataHTML);
+
 
   }
-getData(showData);
+  $('.search-form').submit(function(event){
+    $('.js-container').empty();
+    $('.js-titleContainer').empty();
+    event.preventDefault();
+    let userInput = $('#search-box').val();
+    let userCnt = $('#cntSelector').val();
+    let userUnits=$('#unitSelector').val();
+    getData(userInput, userCnt, userUnits, showData);
+    console.log(today);
+  })
+
+//Render function
+// getData(showData);
 })
+//object.list[0] = today
+//list[0].temp.day = today's day temp
+
+
+//obj.list.weather[0].description
