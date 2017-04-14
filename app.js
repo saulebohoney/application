94012fdd1db17c622dced6c9a9e74187e0636a6e
Harrison wright <Harrison wright>
2017-04-14 12:17:21 -0500

    working on auto-complete

--- app.js
+++ app.js
@@ -64,4 +64,35 @@ $(function(){
     console.log(today);
   })
 
+    $(function () 
+ {
+   $("#search-box").autocomplete({
+    source: function (request, response) {
+     $.getJSON(
+      "http://gd.geobytes.com/AutoCompleteCity?callback=?&q="+request.term,
+      function (data) {
+       response(data);
+      }
+     );
+    },
+    minLength: 3,
+    select: function (event, ui) {
+     var selectedObj = ui.item;
+     $("#search-box").val(selectedObj.value);
+    getcitydetails(selectedObj.value);
+     return false;
+    },
+    open: function () {
+     $(this).removeClass("ui-corner-all").addClass("ui-corner-top");
+    },
+    close: function () {
+     $(this).removeClass("ui-corner-top").addClass("ui-corner-all");
+    }
+   });
+   $("#search-box").autocomplete("option", "delay", 100);
+  });
+
+
+
+
 })
